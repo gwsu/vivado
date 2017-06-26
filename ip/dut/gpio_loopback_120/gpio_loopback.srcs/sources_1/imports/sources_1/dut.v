3e183adf759f8b9d24572f83d98760c08c256b5e
gwsu <g_wsu@sina.com>
2017-06-26 17:39:48 -0400

    add 120pin test

---
+++ ip/dut/gpio_loopback_120/gpio_loopback.srcs/sources_1/imports/sources_1/dut.v
@@ -0,0 +1,335 @@
+///////////////////////////////////////////////////////////////////
+//author: wesleyguo
+//date  : 20170620
+//function: for gpio loopback test, fsm and pin 
+//version: v10
+///////////////////////////////////////////////////////////////////
+
+module dut_120
+(
+input  wire           clk,
+input  wire           rst,
+
+input  wire [31:0]    addr,
+input  wire [31:0]    data_in,
+input  wire [3: 0]    we,
+input  wire           en,
+output reg  [31:0]    data_out,
+
+input  wire [119:0]   a_in,
+output reg            a_oe,
+input  wire [119:0]   b_in,
+output reg            b_oe,
+
+output reg  [119:0]   a_out,
+output reg  [119:0]   b_out
+);
+
+//#############################################################################################
+
+    parameter IDEL = 3'b000;
+    
+    parameter A2B1 = 3'b001;
+    parameter A2B0 = 3'b010;
+    
+    parameter B2A1 = 3'b011;
+    parameter B2A0 = 3'b100;
+    
+    parameter A01B = 3'b101;
+    parameter B01A = 3'b110;
+        
+    
+    
+    parameter ENDL = 3'b111;
+     
+//############################################################################################# 
+   
+   reg [2:0] state = IDEL;   
+   always @(posedge clk or posedge rst)
+   begin
+      if (rst) begin
+         state <= IDEL;
+      end
+      else
+         case (state)
+            IDEL : begin
+               if (reg_start)
+                  state <= A2B1;
+               else       
+                  state <= state;
+               end
+            A2B1 : begin
+               if (a2b1_dealy)
+                   state <= A2B0;
+               else 
+                   state <= state;
+               end
+            A2B0 : begin
+               if (a2b0_dealy)
+                   state <= B2A1 ;
+               else 
+                   state <= state;
+               end
+            B2A1 : begin
+               if (b2a1_dealy)
+                   state <= B2A0;
+               else 
+                   state <= state;
+               end
+            B2A0 : begin
+               if (b2a0_dealy)
+                   state <= A01B ;
+               else 
+                   state <= state;
+               end
+            A01B : begin
+                  if (b01a_dealy)
+                      state <= B01A;
+                  else 
+                      state <= state;
+                  end
+            B01A : begin
+                  if (b01a_dealy)
+                      state <= ENDL ;
+                  else 
+                      state <= state;
+                  end
+            
+            ENDL: begin               
+                  state <= IDEL ;
+               end          
+          endcase
+    end 
+//#############################################################################################    
+ // flow control   
+   
+   reg  [31 : 0]  reg_start;
+    
+   always @(posedge clk or posedge rst) 
+   begin
+        if (rst)
+            reg_start <= 32'd0;
+        else begin
+            if ( ( addr==32'd0) && ( en==1'd1 ) && ( we==4'hf ) )
+            reg_start <= data_in;
+            else
+            reg_start <= 32'd0;
+        end
+   end 
+  
+// a2b0 and dealy
+   reg [31 : 0]  a2b0_dealy_counter;
+   
+   always @(posedge clk or posedge rst) 
+   begin
+        if (rst)
+            a2b0_dealy_counter <= 1'd0;
+        else begin
+            if (state == A2B0)
+            a2b0_dealy_counter <= a2b0_dealy_counter + 32'd1;
+            else
+            a2b0_dealy_counter <= 32'd0;
+        end
+   end
+   
+   assign a2b0_dealy=a2b0_dealy_counter[3];
+   
+ // a2b1 and dealy  
+   reg [31 : 0]  a2b1_dealy_counter;
+      
+      always @(posedge clk or posedge rst) 
+      begin
+           if (rst)
+               a2b1_dealy_counter <= 1'd0;
+           else begin
+               if (state == A2B1)
+               a2b1_dealy_counter <= a2b1_dealy_counter + 32'd1;
+               else
+               a2b1_dealy_counter <= 32'd0;
+           end
+      end
+      assign a2b1_dealy=a2b1_dealy_counter[3];
+
+     
+//b2a1 and dealy     
+     reg [31 : 0]  b2a1_dealy_counter;
+       
+     always @(posedge clk or posedge rst) 
+     begin
+          if (rst)
+              b2a1_dealy_counter <= 1'd0;
+          else begin
+              if (state == B2A1)
+              b2a1_dealy_counter <= b2a1_dealy_counter + 32'd1;
+              else
+              b2a1_dealy_counter <= 32'd0;
+          end
+     end
+     
+     assign b2a1_dealy=b2a1_dealy_counter[3];
+
+//b2a0 and dealy       
+     reg [31 : 0]  b2a0_dealy_counter;
+            
+     always @(posedge clk or posedge rst) 
+     begin
+          if (rst)
+              b2a0_dealy_counter <= 1'd0;
+          else begin
+              if (state == B2A0)
+              b2a0_dealy_counter <= b2a0_dealy_counter + 32'd1;
+              else
+              b2a0_dealy_counter <= 32'd0;
+          end
+     end
+     
+     assign b2a0_dealy=b2a0_dealy_counter[3];
+     
+//b01a and dealy     
+     reg [31 : 0]  b01a_dealy_counter;
+       
+     always @(posedge clk or posedge rst) 
+     begin
+          if (rst)
+              b01a_dealy_counter <= 1'd0;
+          else begin
+              if (state == B2A1)
+              b01a_dealy_counter <= b01a_dealy_counter + 32'd1;
+              else
+              b01a_dealy_counter <= 32'd0;
+          end
+     end
+     
+     assign b01a_dealy=b01a_dealy_counter[3];
+
+//a01b and dealy       
+     reg [31 : 0]  a01b_dealy_counter;
+            
+     always @(posedge clk or posedge rst) 
+     begin
+          if (rst)
+              a01b_dealy_counter <= 1'd0;
+          else begin
+              if (state == B2A0)
+              a01b_dealy_counter <= a01b_dealy_counter + 32'd1;
+              else
+              a01b_dealy_counter <= 32'd0;
+          end
+     end
+     
+     assign a01b_dealy=a01b_dealy_counter[3];              
+//#############################################################################################
+  
+// a_out and b_in sample data  
+   reg  [119:0]  data_bin_0;
+   reg  [119:0]  data_bin_1;
+   reg  [119:0]  data_bin_5;
+   always @(posedge clk or posedge rst)
+   begin
+        if (rst)
+        begin
+           a_out <= 120'd0;
+           a_oe  <= 1'b0;
+           data_bin_0 <= 120'd0;
+           data_bin_1 <= 120'd0;
+        end
+        else begin
+           if   ( state == A2B1 ) begin
+             a_out <= {30'h3fff_ffff,30'h3fff_ffff,30'h3fff_ffff,30'h3fff_ffff};
+             a_oe  <= 1'd1;
+             data_bin_1 <= b_in;
+           end 
+           else if (state == A2B0) begin
+             a_out <= {30'h0000_0000,30'h0000_0000,30'h0000_0000,30'h0000_0000};
+             a_oe  <= 1'd1;
+             data_bin_0 <= b_in;
+           end 
+           else if ( state == A01B) begin
+             b_out <= {30'h5555_5555,30'h5555_5555,30'h5555_5555,30'h5555_5555};
+             b_oe  <= 1'd1;
+             data_ain_5 <= a_in; 
+           end          
+           else begin
+             a_oe <= 1'd0; 
+           end        
+        end
+   end 
+   
+// b_out and a_in sample data
+   
+   reg  [119: 0] data_ain_0;
+   reg  [119: 0] data_ain_1; 
+   reg  [119: 0] data_ain_5;   
+   always @(posedge clk or posedge rst)
+   begin
+        if (rst)
+        begin
+           b_out <= 120'd0;
+           b_oe  <= 1'b0;
+           data_ain_1 <= 120'd0;
+           data_ain_0 <= 120'd0;
+        end
+        else begin
+           if   ( state == B2A1 ) begin
+             b_out <= {30'h3fff_ffff,30'h3fff_ffff,30'h3fff_ffff,30'h3fff_ffff};
+             b_oe  <= 1'd1;
+             data_ain_1 <= a_in;
+           end 
+           else if ( state == B2A0) begin
+             b_out <= {30'h0000_0000,30'h0000_0000,30'h0000_0000,30'h0000_0000};
+             b_oe  <= 1'd1;
+             data_ain_0 <= a_in;
+           end
+           else if ( state == B01A) begin
+             b_out <= {30'h5555_5555,30'h5555_5555,30'h5555_5555,30'h5555_5555};
+             b_oe  <= 1'd1;
+             data_ain_5 <= a_in;
+           end           
+           else begin
+             b_oe <= 1'd0; 
+           end                   
+        end
+   end 
+ //#############################################################################################
+    wire [119: 0]    a_result;
+    wire [119: 0]    b_result; 
+ 
+    always @(posedge clk or posedge rst) 
+    begin
+      if (rst)
+          data_out <= 32'd0;
+      else begin
+          if (en==1'd1 && we==4'h00 )begin
+                case(addr)
+                    // read ain to bus
+                    32'd4:
+                    data_out <= {6'd0,data_ain_1};
+                    //read ain-0 to bus
+                    32'd8:
+                    data_out <= {6'd0,data_ain_0};
+                    // read bin-1 to bus
+                    32'd12:
+                    data_out <= {6'd0,data_bin_1};
+                    // read bin-0 to bus
+                    32'd16:
+                    data_out <= {6'd0,data_bin_0};                    
+                    // read apin result to bus                    
+                    32'd20:
+                    data_out <= {a_result};
+                    // read bpin result to bus
+                    32'd24:
+                    data_out <= {b_result};                                    
+                    default:
+                    data_out <= 32'd0;   
+                endcase
+          end           
+      end
+    end 
+ 
+ assign a_result  = (data_ain_1 ^ {26'h3fff_ffff,30'h3fff_ffff,30'h3fff_ffff,30'h3fff_ffff}) | (data_ain_0 ^ 120'd0);
+ assign b_result  = (data_bin_1 ^ {30'h3fff_ffff,30'h3fff_ffff,30'h3fff_ffff,30'h3fff_ffff}) | (data_bin_0 ^ 120'd0);
+  
+ //#############################################################################################
+   
+   
+ endmodule
---
+++ ip/dut/jack_120/jack/jack.runs/impl_1/jack_120.tcl
@@ -0,0 +1,134 @@
+proc start_step { step } {
+  set stopFile ".stop.rst"
+  if {[file isfile .stop.rst]} {
+    puts ""
+    puts "*** Halting run - EA reset detected ***"
+    puts ""
+    puts ""
+    return -code error
+  }
+  set beginFile ".$step.begin.rst"
+  set platform "$::tcl_platform(platform)"
+  set user "$::tcl_platform(user)"
+  set pid [pid]
+  set host ""
+  if { [string equal $platform unix] } {
+    if { [info exist ::env(HOSTNAME)] } {
+      set host $::env(HOSTNAME)
+    }
+  } else {
+    if { [info exist ::env(COMPUTERNAME)] } {
+      set host $::env(COMPUTERNAME)
+    }
+  }
+  set ch [open $beginFile w]
+  puts $ch "<?xml version=\"1.0\"?>"
+  puts $ch "<ProcessHandle Version=\"1\" Minor=\"0\">"
+  puts $ch "    <Process Command=\".planAhead.\" Owner=\"$user\" Host=\"$host\" Pid=\"$pid\">"
+  puts $ch "    </Process>"
+  puts $ch "</ProcessHandle>"
+  close $ch
+}
+
+proc end_step { step } {
+  set endFile ".$step.end.rst"
+  set ch [open $endFile w]
+  close $ch
+}
+
+proc step_failed { step } {
+  set endFile ".$step.error.rst"
+  set ch [open $endFile w]
+  close $ch
+}
+
+set_msg_config -id {HDL 9-1061} -limit 100000
+set_msg_config -id {HDL 9-1654} -limit 100000
+
+start_step init_design
+set ACTIVE_STEP init_design
+set rc [catch {
+  create_msg_db init_design.pb
+  set_property design_mode GateLvl [current_fileset]
+  set_param project.singleFileAddWarning.threshold 0
+  set_property webtalk.parent_dir /home/wesleyguo/github/vivado/vivado/ip/dut/jack_120/jack/jack.cache/wt [current_project]
+  set_property parent.project_path /home/wesleyguo/github/vivado/vivado/ip/dut/jack_120/jack/jack.xpr [current_project]
+  set_property ip_repo_paths {
+  /home/wesleyguo/github/vivado/vivado/ip/dut/jack_120/jack/jack.srcs/sources_1/imports/sources_1
+  /home/wesleyguo/github/vivado/vivado/b2000t_jtag_axi_loopback/b2000t_jtag_axi_loopback/b2000t_jtag_axi_loopback.srcs/sources_1
+} [current_project]
+  set_property ip_output_repo /home/wesleyguo/github/vivado/vivado/ip/dut/jack_120/jack/jack.cache/ip [current_project]
+  set_property ip_cache_permissions {read write} [current_project]
+  add_files -quiet /home/wesleyguo/github/vivado/vivado/ip/dut/jack_120/jack/jack.runs/synth_1/jack_120.dcp
+  link_design -top jack_120 -part xc7v2000tflg1925-1
+  write_hwdef -file jack_120.hwdef
+  close_msg_db -file init_design.pb
+} RESULT]
+if {$rc} {
+  step_failed init_design
+  return -code error $RESULT
+} else {
+  end_step init_design
+  unset ACTIVE_STEP 
+}
+
+start_step opt_design
+set ACTIVE_STEP opt_design
+set rc [catch {
+  create_msg_db opt_design.pb
+  opt_design 
+  write_checkpoint -force jack_120_opt.dcp
+  catch { report_drc -file jack_120_drc_opted.rpt }
+  close_msg_db -file opt_design.pb
+} RESULT]
+if {$rc} {
+  step_failed opt_design
+  return -code error $RESULT
+} else {
+  end_step opt_design
+  unset ACTIVE_STEP 
+}
+
+start_step place_design
+set ACTIVE_STEP place_design
+set rc [catch {
+  create_msg_db place_design.pb
+  implement_debug_core 
+  place_design 
+  write_checkpoint -force jack_120_placed.dcp
+  catch { report_io -file jack_120_io_placed.rpt }
+  catch { report_utilization -file jack_120_utilization_placed.rpt -pb jack_120_utilization_placed.pb }
+  catch { report_control_sets -verbose -file jack_120_control_sets_placed.rpt }
+  close_msg_db -file place_design.pb
+} RESULT]
+if {$rc} {
+  step_failed place_design
+  return -code error $RESULT
+} else {
+  end_step place_design
+  unset ACTIVE_STEP 
+}
+
+start_step route_design
+set ACTIVE_STEP route_design
+set rc [catch {
+  create_msg_db route_design.pb
+  route_design 
+  write_checkpoint -force jack_120_routed.dcp
+  catch { report_drc -file jack_120_drc_routed.rpt -pb jack_120_drc_routed.pb -rpx jack_120_drc_routed.rpx }
+  catch { report_methodology -file jack_120_methodology_drc_routed.rpt -rpx jack_120_methodology_drc_routed.rpx }
+  catch { report_timing_summary -warn_on_violation -max_paths 10 -file jack_120_timing_summary_routed.rpt -rpx jack_120_timing_summary_routed.rpx }
+  catch { report_power -file jack_120_power_routed.rpt -pb jack_120_power_summary_routed.pb -rpx jack_120_power_routed.rpx }
+  catch { report_route_status -file jack_120_route_status.rpt -pb jack_120_route_status.pb }
+  catch { report_clock_utilization -file jack_120_clock_utilization_routed.rpt }
+  close_msg_db -file route_design.pb
+} RESULT]
+if {$rc} {
+  write_checkpoint -force jack_120_routed_error.dcp
+  step_failed route_design
+  return -code error $RESULT
+} else {
+  end_step route_design
+  unset ACTIVE_STEP 
+}
+
